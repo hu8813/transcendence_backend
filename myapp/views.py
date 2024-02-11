9c419c9ae9f38bab784b2d3641639ef966301eb9
Huseyin Kaya Aydin <huaydin@c4r1p7.42vienna.com>
2024-02-11 18:09:42 +0100

    a

--- myapp/views.py
+++ myapp/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-
+from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 
+@csrf_exempt
 def get_csrf_token(request):
     # Get the CSRF token from the request's CSRF middleware
     csrf_token = request.COOKIES.get('csrftoken', '')
 
     # Return the CSRF token in a JSON response
     return JsonResponse({'csrfToken': csrf_token})
-    
+
+@csrf_exempt    
 def login_view(request):
     # Handle login form submission and authentication logic here
     if request.method == 'POST':
