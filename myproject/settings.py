8d8fcde7ca7f3cebdc98d7f70b0a9f0f2c6b8b38
Huseyin Kaya Aydin <huaydin@c4r1p4.42vienna.com>
2024-02-19 17:48:40 +0100

    a

--- myproject/settings.py
+++ myproject/settings.py
@@ -141,7 +141,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-CSRF_TRUSTED_ORIGINS = ['https://transcendence-beige.vercel.app','https://zc5vlf-3000.csb.app','y494mt-3000.csb.app']
+CSRF_TRUSTED_ORIGINS = ['https://transcendence-beige.vercel.app','https://zc5vlf-3000.csb.app','https://y494mt-3000.csb.app']
 
 
 # Static files (CSS, JavaScript, Images)
