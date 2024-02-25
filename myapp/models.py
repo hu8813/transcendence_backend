0f0862d8fbdeb6d696e86bb6c81d7757fb251bec
Huseyin Kaya Aydin <huaydin@c4r1p4.42vienna.com>
2024-02-25 11:59:46 +0100

    a

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class User(AbstractUser):
+    score = models.IntegerField(default=0)
+    
 # Create your models here.
 class YourModel(models.Model):
     # your existing fields
